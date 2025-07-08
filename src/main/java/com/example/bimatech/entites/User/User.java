8a593b34f2f10c3ca62cdbd37316b5da97af9d4f
Hadylfriaa <friaahadil@gmail.com>
2025-07-08 10:54:52 +0100

    cree project

--- src/main/java/com/example/bimatech/entites/User/User.java
+++ src/main/java/com/example/bimatech/entites/User/User.java
@@ -1,4 +1,24 @@
 package com.example.bimatech.entites.User;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
 }
