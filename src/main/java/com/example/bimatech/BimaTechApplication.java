6aab1110595593e843d95a5091cf7b0e525b0507
Hadylfriaa <friaahadil@gmail.com>
2025-07-08 09:14:13 +0100

    cree project

---
+++ src/main/java/com/example/bimatech/BimaTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.bimatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimaTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bimatech/BimaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bimatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimaTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
